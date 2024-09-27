cce17dda18799ae5e39893e5b5d25ae37205142f
derciosinione <derciosinione@gmail.com>
2024-09-27 11:29:50 +0100

    ªFeat: Implemented Extension to ClaimsPrincipal so that i am able to convert claims to model

---
+++ Application/Models/AuthUserClaims.cs
@@ -0,0 +1,17 @@
+namespace Application.Models;
+
+public record AuthUserClaims
+{
+    public string? Id { get; set; }
+    public string? Email { get; set; }
+    public string? Perfil { get; set; }
+    public string? MPerfil { get; set; }
+    public string? IdRegiao { get; set; }
+    public string? GuidRegion { get; set; }
+    public string? Idc { get; set; }
+    public string? MDirector { get; set; }
+    public bool IsMember { get; set; }
+    public string? Pais { get; set; }
+    public string? Moeda { get; set; }
+    public string? Role { get; set; }
+}
